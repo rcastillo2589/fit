38315dc934fcac58791fea80b7c8f5c78760577a
Robert Castillo <rcastillo2589@gmail.com>
2018-04-23 22:21:06 -0500

    Refactored goals route for async\await

--- fit.goals.api/src/route/v1/goals-route.js
+++ fit.goals.api/src/route/v1/goals-route.js
@@ -6,89 +6,81 @@ async function goalsRoute(fastify, options) {
   // Get all goals for the provided user
   fastify.get('/goals/user/:id', async(request, reply) => {
     const userId = request.params['id'];
-    await inject(DaoFactory)
-      .getGoalsDao()
-      .getGoalsByUserId(userId)
-      .then((data) => {
-        reply.send(data);
-      }).catch((err) => {
-        reply
-          .code(500)
-          .send('Something happened');
-      });
+    try {
+      let result = await inject(DaoFactory)
+        .getGoalsDao()
+        .getGoalsByUserId(userId);
+      reply.send(result);
+    } catch(err) {
+      reply.code(500).send(err);
+    }
   });
 
   // Get one goal by id
   fastify.get('/goal/:id', async(request, reply) => {
-    let goalId = new ObjectId(request.params["id"]);
-    await inject(DaoFactory)
-      .getGoalsDao()
-      .getGoalById(goalId)
-      .then((data) => {
-        reply.send(data);
-      }).catch((err) => {
-        reply
-          .code(500)
-          .send('Something happened');
-      });
+    const goalId = new ObjectId(request.params["id"]);
+    try {
+      let result = await inject(DaoFactory)
+        .getGoalsDao()
+        .getGoalById(goalId);
+      reply.send(result);
+    } catch(err) {
+      reply.code(500).send(err);
+    }
   });
 
   // Create one goal for the provided user
   fastify.post('/goal', async(request, reply) => {
     const user = request.body.user;
     const goal = request.body.goal;
-    await inject(DaoFactory)
-      .getGoalsDao()
-      .createGoal(user, goal)
-      .then((data) => {
-        reply.send(data);
-      }).catch((err) => {
-        reply.code(500)
-          .send('Something happened');
-      });
+    try {
+      let result = await inject(DaoFactory)
+        .getGoalsDao()
+        .createGoal(user, goal);
+      reply.send(result);
+    } catch(err) {
+      reply.code(500).send(err);
+    }
   });
 
   // Create a progress mark
   fastify.post('/goal/progress', async(request, reply) => {
     let goalId = new ObjectId(request.body.goalId);
     let progress = request.body.progress;
-    await inject(DaoFactory)
-      .getGoalsDao()
-      .addProgressMark(goalId, progress)
-      .then((data) => {
-        reply.send(data);
-      }).catch((err) => {
-        reply.code(500)
-          .send('Something happened');
-      })
+    try {
+      let result = await inject(DaoFactory)
+        .getGoalsDao()
+        .addProgressMark(goalId, progress);
+      reply.send(result);
+    } catch(err) {
+      reply.code(500).send(err);
+    }
   });
 
   // Delete all goals for the provided user
   fastify.delete('/goal/user/:id', async(request, reply) => {
     const userId = request.params['id'];
-    await inject(DaoFactory)
-      .getGoalsDao()
-      .deleteAllGoalsByUser(userId)
-      .then((data) => {
-        reply.send(data);
-      }).catch((err) => {
-        reply.code(500)
-          .send('Something happened');
-      });
+    try {
+      let result = await inject(DaoFactory)
+        .getGoalsDao()
+        .deleteAllGoalsByUser(userId);
+      reply.send(result);
+    } catch(err) {
+      reply.code(500).send(err);
+    }
   });
 
   // Delete a single goal for by id
   fastify.delete('/goal/:id', async(request, reply) => {
     const goalId = new ObjectId(request.params['id']);
-    await inject(DaoFactory)
-      .getGoalsDao()
-      .deleteGoalById(goalId)
-      .then((data) => {
-        reply.send(data);
-      }).catch((err) => {
-        reply.code(500)
-          .send('Something happened');
-      });
+    try {
+      let result = await inject(DaoFactory)
+        .getGoalsDao()
+        .deleteGoalById(goalId);
+      reply.send(result);
+    } catch(err) {
+      reply.code(500).send(err);
+    }
   });
 }
 
